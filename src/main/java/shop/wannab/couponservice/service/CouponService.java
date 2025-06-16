672c523fda9c0adf739a87468d1aefb12d7ba119
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-06-16 17:23:33 +0900

    feat: 생일 쿠폰 발급 기능

--- src/main/java/shop/wannab/couponservice/service/CouponService.java
+++ src/main/java/shop/wannab/couponservice/service/CouponService.java
@@ -1,9 +1,13 @@
 package shop.wannab.couponservice.service;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import shop.wannab.couponservice.domain.Coupon;
 import shop.wannab.couponservice.domain.CouponPolicy;
 import shop.wannab.couponservice.domain.enums.CouponStatus;
@@ -15,10 +19,12 @@ import shop.wannab.couponservice.repository.CouponRepository;
 public class CouponService {
     private final CouponRepository couponRepository;
     private final CouponPolicyRepository couponPolicyRepository;
+    private final RestTemplate restTemplate;
 
-    public CouponService(CouponRepository couponRepository, CouponPolicyRepository couponPolicyRepository) {
+    public CouponService(CouponRepository couponRepository, CouponPolicyRepository couponPolicyRepository, RestTemplate restTemplate) {
         this.couponRepository = couponRepository;
         this.couponPolicyRepository = couponPolicyRepository;
+        this.restTemplate = restTemplate;
     }
 
     @Transactional
@@ -49,6 +55,44 @@ public class CouponService {
         }
 
         saveNewCoupon(userId,couponPolicy,"CST");
+    }
+
+    @Transactional
+    public void issueBirthdayCoupon(int month) {
+        System.out.println("생일 쿠폰 발급 로직 시작 (월: " + month + ")");
+
+        CouponPolicy birthdayPolicy = couponPolicyRepository.findByPolicyRule(PolicyRule.BIRTHDAY);
+        if (birthdayPolicy == null) {
+            throw new IllegalArgumentException("해당 쿠폰이 없습니다.");
+        }
+        //향후 경로 맞출것
+        String userServiceBaseUrl = "http://localhost:8082";
+
+        String requestUrl = UriComponentsBuilder.fromHttpUrl(userServiceBaseUrl)
+                .path("/api/users/birthdays")
+                .queryParam("month", month)
+                .toUriString();
+
+        List<Long> birthdayUserIds;
+
+        try {
+            Long[] userIdsArray = restTemplate.getForObject(requestUrl, Long[].class);
+            if (userIdsArray == null) {
+                birthdayUserIds = List.of();
+            } else {
+                birthdayUserIds = Arrays.asList(userIdsArray);
+            }
+        } catch (Exception e) {
+            birthdayUserIds = List.of();
+        }
+
+        for (Long userId : birthdayUserIds) {
+            try {
+                saveNewCoupon(userId, birthdayPolicy,"BD");
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("생일 쿠폰 발급 실패");
+            }
+        }
     }
 
     private void saveNewCoupon(Long userId, CouponPolicy couponPolicy,String prefix) {
