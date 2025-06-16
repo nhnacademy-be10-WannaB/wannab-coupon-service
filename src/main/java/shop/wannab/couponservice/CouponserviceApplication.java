4f0d6b633909824411a68c909170596ead05a517
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-06-16 17:22:52 +0900

    feat: 생일 쿠폰 발급 기능 @EnableScheduling

--- src/main/java/shop/wannab/couponservice/CouponserviceApplication.java
+++ src/main/java/shop/wannab/couponservice/CouponserviceApplication.java
@@ -2,8 +2,10 @@ package shop.wannab.couponservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CouponserviceApplication {
 
 	public static void main(String[] args) {
