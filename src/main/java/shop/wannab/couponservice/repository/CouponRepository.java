763a60dd027caf4c555706b909d737420cdd5489
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-06-16 15:55:19 +0900

    refactor: 불필요한 검증 삭제

--- src/main/java/shop/wannab/couponservice/repository/CouponRepository.java
+++ src/main/java/shop/wannab/couponservice/repository/CouponRepository.java
@@ -1,11 +1,10 @@
 package shop.wannab.couponservice.repository;
 
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.wannab.couponservice.domain.Coupon;
 import shop.wannab.couponservice.domain.CouponPolicy;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
-    Coupon findByUserIdAndCouponPolicy(@NotNull Long userId, CouponPolicy couponPolicy);
-    boolean existsByUserIdAndCouponPolicy(@NotNull Long userId, CouponPolicy couponPolicy);
+    Coupon findByUserIdAndCouponPolicy(Long userId, CouponPolicy couponPolicy);
+    boolean existsByUserIdAndCouponPolicy(Long userId, CouponPolicy couponPolicy);
 }
