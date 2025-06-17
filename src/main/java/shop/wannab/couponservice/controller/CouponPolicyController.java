5ee97d5595e4dde8b2225908e1a808538ae88efb
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-06-17 14:57:08 +0900

    refactor: 패키지 구조 변경

--- src/main/java/shop/wannab/couponservice/controller/CouponPolicyController.java
+++ src/main/java/shop/wannab/couponservice/controller/CouponPolicyController.java
@@ -14,10 +14,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shop.wannab.couponservice.domain.CouponPolicy;
-import shop.wannab.couponservice.domain.dto.CouponPolicyDetailResponseDto;
-import shop.wannab.couponservice.domain.dto.CouponPolicyResponseDto;
-import shop.wannab.couponservice.domain.dto.CreateCouponPolicyDto;
-import shop.wannab.couponservice.domain.dto.UpdateCouponPolicyDto;
+import shop.wannab.couponservice.domain.couponpolicy.dto.CouponPolicyDetailResponseDto;
+import shop.wannab.couponservice.domain.couponpolicy.dto.CouponPolicyResponseDto;
+import shop.wannab.couponservice.domain.couponpolicy.dto.CreateCouponPolicyDto;
+import shop.wannab.couponservice.domain.couponpolicy.dto.UpdateCouponPolicyDto;
 import shop.wannab.couponservice.service.CouponPolicyService;
 
 
