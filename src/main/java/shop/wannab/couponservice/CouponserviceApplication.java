394c43c88111d7381ef4c37091365e6fea882f76
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-06-20 22:08:36 +0900

    feat: Eureka 설정 추가

--- src/main/java/shop/wannab/couponservice/CouponserviceApplication.java
+++ src/main/java/shop/wannab/couponservice/CouponserviceApplication.java
@@ -2,10 +2,12 @@ package shop.wannab.couponservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class CouponserviceApplication {
 
 	public static void main(String[] args) {
