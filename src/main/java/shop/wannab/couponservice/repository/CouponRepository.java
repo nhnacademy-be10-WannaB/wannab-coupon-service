b25b1c2f84a13f1a37a26510985a65b92a71ff7f
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-06-29 17:04:11 +0900

    refactor: 쿠폰 조회 페이지네이션 추가

--- src/main/java/shop/wannab/couponservice/repository/CouponRepository.java
+++ src/main/java/shop/wannab/couponservice/repository/CouponRepository.java
@@ -1,6 +1,8 @@
 package shop.wannab.couponservice.repository;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.wannab.couponservice.domain.coupon.Coupon;
 import shop.wannab.couponservice.domain.couponpolicy.CouponPolicy;
@@ -11,4 +13,6 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
     boolean existsByUserIdAndCouponPolicy(Long userId, CouponPolicy couponPolicy);
     List<Coupon> findByUserId(Long userId);
     List<Coupon> findByUserIdAndStatus(Long userId, CouponStatus status);
+    Page<Coupon> findByUserId(Long userId, Pageable pageable);
+
 }
