03f33aabb2d4dfec1fe221a9e8398c290f66a629
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-07-09 13:10:24 +0900

    rename: X-User-Id -> X-USER-ID

--- src/main/java/shop/wannab/couponservice/coupon/controller/CouponController.java
+++ src/main/java/shop/wannab/couponservice/coupon/controller/CouponController.java
@@ -18,8 +18,8 @@ import shop.wannab.couponservice.coupon.dto.PageResponseDto;
 import shop.wannab.couponservice.coupon.dto.TryApplyCouponsRequestDto;
 import shop.wannab.couponservice.coupon.dto.TryApplyCouponsResponseDto;
 import shop.wannab.couponservice.coupon.service.CouponService;
-import shop.wannab.couponservice.couponpolicy.service.CouponPolicyService;
 import shop.wannab.couponservice.couponpolicy.dto.IssuableCouponPolicyDto;
+import shop.wannab.couponservice.couponpolicy.service.CouponPolicyService;
 
 @RestController
 @RequestMapping("/api/coupons")
@@ -34,7 +34,7 @@ public class CouponController {
 
     @PostMapping("/issue/welcome")
     public ResponseEntity<String> issueWelcomeCouponForNewUser(
-            @RequestHeader("X-User-Id") Long userId) {
+            @RequestHeader("X-USER-ID") Long userId) {
         try {
             couponService.issueWelcomeCouponForNewUser(userId);
             return ResponseEntity.ok("웰컴 쿠폰이 성공적으로 발급되었습니다.");
@@ -45,7 +45,7 @@ public class CouponController {
 
     @PostMapping("/issue/custom")
     public ResponseEntity<String> issueCustomCoupon(
-            @RequestHeader("X-User-Id") Long userId,
+            @RequestHeader("X-USER-ID") Long userId,
             @RequestParam Long couponPolicyId){
         couponService.issueGeneralCoupon(userId, couponPolicyId);
         return ResponseEntity.ok("쿠폰이 성공적으로 발급되었습니다.");
@@ -60,7 +60,7 @@ public class CouponController {
 
     @GetMapping("/me")
     public ResponseEntity<PageResponseDto<CouponResponseToUserDto>> getCouponsForUser(
-            @RequestHeader("X-User-Id") Long userId,
+            @RequestHeader("X-USER-ID") Long userId,
             Pageable pageable) {
 
         // 자신의 서비스 로직을 호출하여 결과를 가져옵니다.
@@ -72,14 +72,14 @@ public class CouponController {
 
     @PostMapping("/order")
     public ResponseEntity<ApplicableCouponsDto> getApplicableCoupons(
-            @RequestHeader("X-User-Id") Long userId,
+            @RequestHeader("X-USER-ID") Long userId,
             @RequestBody OrderCouponsRequestDto orderCouponsRequestDtoList){
         return ResponseEntity.ok(couponService.getUserApplicableCoupons(userId, orderCouponsRequestDtoList));
     }
 
     @PostMapping("/order/apply")
     public ResponseEntity<List<TryApplyCouponsResponseDto>> getApplyCoupons(
-            @RequestHeader("X-User-Id") Long userId,
+            @RequestHeader("X-USER-ID") Long userId,
             @RequestBody TryApplyCouponsRequestDto tryApplyCouponsRequestDtoMap
             ){
         return ResponseEntity.ok(couponService.applyCoupons(userId, tryApplyCouponsRequestDtoMap));
@@ -87,7 +87,7 @@ public class CouponController {
 
     @PostMapping("/order/success")
     public ResponseEntity<Void> processUsedCoupons(
-            @RequestHeader("X-User-Id") Long userId,
+            @RequestHeader("X-USER-ID") Long userId,
             @RequestBody CouponUsageRequestDto requestDto) {
 
         couponService.processUsedCoupons(userId, requestDto);
