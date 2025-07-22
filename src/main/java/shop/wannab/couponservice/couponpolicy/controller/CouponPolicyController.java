8d577b3f23107c88eb15b4fccaeb09df861c4bcf
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-07-22 09:41:36 +0900

    refactor: 불필요한 코드 삭제

--- src/main/java/shop/wannab/couponservice/couponpolicy/controller/CouponPolicyController.java
+++ src/main/java/shop/wannab/couponservice/couponpolicy/controller/CouponPolicyController.java
@@ -2,7 +2,6 @@ package shop.wannab.couponservice.couponpolicy.controller;
 
 
 import jakarta.validation.Valid;
-import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shop.wannab.couponservice.category.CategoryService;
 import shop.wannab.couponservice.category.dto.CategoryHierarchyDto;
-import shop.wannab.couponservice.coupon.service.CouponService;
 import shop.wannab.couponservice.couponpolicy.dto.CouponPageDataDto;
 import shop.wannab.couponservice.couponpolicy.dto.CouponPolicyResponseDto;
 import shop.wannab.couponservice.couponpolicy.dto.CreateCouponPolicyDto;
@@ -29,7 +27,6 @@ public class CouponPolicyController {
 
     private final CouponPolicyService couponPolicyService;
     private final CategoryService categoryService;
-    private final CouponService couponService;
 
     @PostMapping
     public ResponseEntity<Void> createCouponPolicy(@Valid @RequestBody CreateCouponPolicyDto createCouponPolicyDto) {
@@ -48,13 +45,6 @@ public class CouponPolicyController {
     @DeleteMapping("/{policyId}")
     public ResponseEntity<Void> deleteCouponPolicy(@PathVariable Long policyId) {
         couponPolicyService.deleteCouponPolicyById(policyId);
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping("/issue-birthday")
-    public ResponseEntity<Void> issueBirthdayCoupon(){
-        int month = LocalDate.now().getMonthValue();
-        couponService.issueBirthdayCoupon(month);
         return ResponseEntity.ok().build();
     }
 }
